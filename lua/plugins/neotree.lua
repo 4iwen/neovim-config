f8b46361d58476b87582a8f061251cd8db5bb12e
4iwen <luky.bien@gmail.com>
2023-08-19 23:01:01 +0200

    Changed the always hide setting in neotree and removed unfunctional code

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,10 +7,10 @@ return {
         show_hidden_count = true,
         hide_dotfiles = false,
         hide_gitignored = false,
-        hide_by_name = {
+        never_show = {
           ".git",
+          ".cache",
         },
-        never_show = {},
       },
     },
   },
